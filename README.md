a4e52b80bf1e398a76d31f75c318795c01aa672a
wilsoneng <83547305+wilsoneng@users.noreply.github.com>
2021-07-11 18:42:44 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# 👀 Desenvolvendo um sistema de gerenciamento de pessoas em API REST com Spring Boot
+
+## 🎮️ Sobre
+O projeto é um API REST com Spring Boot para cadastro e gerenciamento de  pessoas de uma organização, até o deploy na nuvem (Heroku).
+
+O projeto foi desenvolvido utilizando as seguintes tecnologias:
+
+💻️Spring boot 💻️java 
+
+## Feito por wilsoneng
---
+++ src/PersonUtils.java
@@ -0,0 +1,37 @@
+package one.digitalinnovation.personapi.utils;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class PersonUtils {
+
+    private static final String FIRST_NAME = "wilson";
+    private static final String LAST_NAME = "santos";
+    private static final String CPF_NUMBER = "456.342.823-45";
+    private static final long PERSON_ID = 1L;
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
+
+    public static PersonDTO createFakeDTO() {
+        return PersonDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate("02-09-2010")
+                .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
+                .build();
+    }
+
+    public static Person createFakeEntity() {
+        return Person.builder()
+                .id(PERSON_ID)
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .cpf(CPF_NUMBER)
+                .birthDate(BIRTH_DATE)
+                .phones(Collections.singletonList(PhoneUtils.createFakeEntity()))
+                .build();
+    }
+}
